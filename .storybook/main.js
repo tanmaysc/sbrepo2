b31f2e76b9d764fd4ea972acf22da463971c4925
tanmaysc <tanmay.k@smarter.codes>
2021-10-01 22:06:43 +0530

    adding ref

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,11 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  "refs": {
+    'test-2': {
+      title: "Storybook test 2",
+      url: "https://615732e13257b1003a92ce13-lhgamnngju.chromatic.com/"
+    }
+   }
 }
